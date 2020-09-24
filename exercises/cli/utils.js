d403306f67fb1315e3af3ddbfb0932322568729b
John Lee <johnjun.lee91@gmail.com>
2020-09-23 21:38:12 -0400

    Answered cli prompts

--- exercises/cli/utils.js
+++ exercises/cli/utils.js
@@ -1,9 +1,15 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const { dirname } = require('path');
+const path = require('path');
 
-const contactsLocation = path.join(__dirname, 'contacts.json')
-const getContacts = () => JSON.parse(fs.readFileSync(contactsLocation))
-const saveContacts = (contacts) => fs.writeFileSync(contactsLocation, JSON.stringify(contacts, null, 2))
+const contactsLocation = path.join(__dirname, 'contacts.json');
 
-module.exports = {contactsLocation, getContacts, saveContacts}
+const getContacts = () => {
+  return JSON.parse(fs.readFileSync(contactsLocation));
+};
 
+const saveContacts = (contacts) => {
+  return fs.writeFileSync(contactsLocation, JSON.stringify(contacts));
+};
+
+module.exports = { contactsLocation, getContacts, saveContacts };
